6e4e39e82f47b59486736ebef426da8db57951e8
刘建超 <31613432+liujianchao1995@users.noreply.github.com>
2023-07-28 14:32:19 +0000

    style:样式调整

--- src/pages/home/Footer.tsx
+++ src/pages/home/Footer.tsx
@@ -7,20 +7,5 @@ import { UserMethods } from "~/types"
 
 export const Footer = () => {
   const t = useT()
-  return (
-    <VStack class="footer" w="$full" py="$4">
-      <HStack spacing="$1">
-        <Anchor href="https://github.com/Xhofe/alist" external>
-          {t("home.footer.powered_by")}
-        </Anchor>
-        <span>|</span>
-        <AnchorWithBase
-          as={Link}
-          href={UserMethods.is_guest(me()) ? "/@login" : "/@manage"}
-        >
-          {t(UserMethods.is_guest(me()) ? "login.login" : "home.footer.manage")}
-        </AnchorWithBase>
-      </HStack>
-    </VStack>
-  )
+  return ""
 }
